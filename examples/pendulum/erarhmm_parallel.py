875b40adc039714303b3ccfbfc4dd422cc5993f0
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2019-09-12 13:44:37 +0200

    update joblib parallelism

--- examples/pendulum/erarhmm_parallel.py
+++ examples/pendulum/erarhmm_parallel.py
@@ -1,35 +1,38 @@
 import autograd.numpy as np
-import autograd.numpy.random as npr
 
 from sds import erARHMM
 from sds.utils import sample_env
 
+import random
 from joblib import Parallel, delayed
 
 
-def create_job(args):
-    obs, act, nb_states, type, nb_iter, prec = args
+def create_job(kwargs):
+    obs = kwargs.pop('obs')
+    act = kwargs.pop('act')
+    nb_states = kwargs.pop('nb_states')
 
-    dm_obs = obs[0].shape[-1]
-    dm_act = act[0].shape[-1]
+    type = kwargs.pop('type')
+    prec = kwargs.pop('prec')
+    nb_iter = kwargs.pop('nb_iter')
 
-    nb_rollouts = len(obs)
-    _train_choice = npr.choice(nb_rollouts, size=int(0.8 * nb_rollouts), replace=False)
-    _train_obs = [obs[i] for i in _train_choice]
-    _train_act = [act[i] for i in _train_choice]
+    train_obs, train_act = zip(*random.sample(list(zip(obs, act)), int(0.8 * len(obs))))
+
+    dm_obs = train_obs[0].shape[-1]
+    dm_act = train_act[0].shape[-1]
 
     erarhmm = erARHMM(nb_states, dm_obs, dm_act, type, learn_ctl=False)
-    erarhmm.initialize(_train_obs, _train_act)
-    lls = erarhmm.em(_train_obs, _train_act, nb_iter=nb_iter, prec=prec, verbose=False)
+    erarhmm.initialize(train_obs, train_act)
+    lls = erarhmm.em(train_obs, train_act, nb_iter=nb_iter, prec=prec, verbose=False)
 
-    mse, norm_mse = erarhmm.kstep_mse(obs, act, horizon=25, stoch=False)
+    mse, norm_mse = erarhmm.kstep_mse(obs, act, horizon=10, stoch=False)
     return erarhmm, lls[-1], mse, norm_mse
 
 
-def parallel_em(obs, act, nb_states, type, nb_iter=50, prec=1e-4, nb_jobs=50):
-    args = [(obs, act, nb_states, type, nb_iter, prec) for _ in range(nb_jobs)]
+def parallel_em(nb_jobs=50, **kwargs):
+    kwargs_list = [kwargs for _ in range(nb_jobs)]
     results = Parallel(n_jobs=-1, verbose=10, backend='loky',
-                       max_nbytes='1000M')(map(delayed(create_job), args))
+                       max_nbytes='1000M')(map(delayed(create_job), kwargs_list))
     erarhmms, lls, mse, norm_mse = list(map(list, zip(*results)))
     return erarhmms, lls, mse, norm_mse
 
@@ -53,11 +56,11 @@ if __name__ == "__main__":
     obs, act = sample_env(env, nb_rollouts, nb_steps)
 
     nb_states = 5
-    models, liklhds, mse, norm_mse = parallel_em(obs=obs, act=act,
+    models, liklhds, mse, norm_mse = parallel_em(nb_jobs=32,
+                                                 obs=obs, act=act,
                                                  nb_states=nb_states,
                                                  type='neural-recurrent',
-                                                 nb_iter=150,
-                                                 prec=1e-4, nb_jobs=32)
+                                                 nb_iter=150, prec=1e-4)
     erarhmm = models[np.argmax(norm_mse)]
 
     print("erarhmm, stochastic, neural")
