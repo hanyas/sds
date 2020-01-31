7af3ac438f8c8634ca2c2acf5a6282028b5c5723
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-01-31 16:11:45 +0100

    update bouncing ball example

--- evaluation/bouncing/bouncing_identify.py
+++ evaluation/bouncing/bouncing_identify.py
@@ -40,30 +40,33 @@ if __name__ == "__main__":
     env = gym.make('BouncingBall-ID-v0')
     env._max_episode_steps = 5000
     env.unwrapped._dt = 0.05
-    env.unwrapped._sigma = 1e-32
+    env.unwrapped._sigma = 1e-16
     env.seed(1337)
 
-    nb_rollouts, nb_steps = 25, 150
     dm_obs = env.observation_space.shape[0]
     dm_act = env.action_space.shape[0]
 
-    obs, act = sample_env(env, nb_rollouts, nb_steps)
+    nb_train_rollouts, nb_train_steps = 25, 150
+    nb_test_rollouts, nb_test_steps = 5, 150
 
-    fig, ax = plt.subplots(nrows=1, ncols=dm_obs + dm_act, figsize=(12, 4))
-    for _obs, _act in zip(obs, act):
-        for k, col in enumerate(ax[:-1]):
-            col.plot(_obs[:, k])
-        ax[-1].plot(_act)
-    plt.show()
+    train_obs, train_act = sample_env(env, nb_train_rollouts, nb_train_steps)
+    test_obs, test_act = sample_env(env, nb_test_rollouts, nb_test_steps)
+
+    # fig, ax = plt.subplots(nrows=1, ncols=dm_obs + dm_act, figsize=(12, 4))
+    # for _obs, _act in zip(obs, act):
+    #     for k, col in enumerate(ax[:-1]):
+    #         col.plot(_obs[:, k])
+    #     ax[-1].plot(_act)
+    # plt.show()
 
     nb_states = 2
 
-    obs_prior = {'mu0': 0., 'sigma0': 1e32, 'nu0': (dm_obs + 1) * 10, 'psi0': 1e-32 * 10}
-    obs_mstep_kwargs = {'use_prior': False}
+    obs_prior = {'mu0': 0., 'sigma0': 1e64, 'nu0': (dm_obs + 1) * 10, 'psi0': 1e-16 * 10}
+    obs_mstep_kwargs = {'use_prior': True}
 
     trans_type = 'neural'
-    trans_prior = {'l2_penalty': 1e-16, 'alpha': 1, 'kappa': 50}
-    trans_kwargs = {'hidden_layer_sizes': (8,),
+    trans_prior = {'l2_penalty': 1e-32, 'alpha': 1, 'kappa': 50}
+    trans_kwargs = {'hidden_layer_sizes': (16,),
                     'norm': {'mean': np.array([0., 0., 0.]),
                              'std': np.array([1., 1., 1.])}}
     trans_mstep_kwargs = {'nb_iter': 25, 'batch_size': 256, 'lr': 1e-3}
@@ -73,35 +76,35 @@ if __name__ == "__main__":
                     obs_prior=obs_prior,
                     trans_prior=trans_prior,
                     trans_kwargs=trans_kwargs)
-    # rarhmm.initialize(obs, act)
+    rarhmm.initialize(train_obs, train_act)
 
-    lls = rarhmm.em(obs, act, nb_iter=100, prec=1e-2, verbose=True,
+    lls = rarhmm.em(train_obs, train_act,
+                    nb_iter=100, prec=1e-2,
                     obs_mstep_kwargs=obs_mstep_kwargs,
                     trans_mstep_kwargs=trans_mstep_kwargs)
 
-    plt.figure(figsize=(5, 5))
-    plt.plot(lls)
-    plt.show()
-
-    plt.figure(figsize=(8, 8))
-    idx = npr.choice(nb_rollouts)
-    _, state = rarhmm.viterbi(obs, act)
-    _seq = npr.choice(len(obs))
-
-    plt.subplot(211)
-    plt.plot(obs[_seq])
-    plt.xlim(0, len(obs[_seq]))
-
-    plt.subplot(212)
-    plt.imshow(state[_seq][None, :], aspect="auto", cmap=cmap, vmin=0, vmax=len(colors) - 1)
-    plt.xlim(0, len(obs[_seq]))
-    plt.ylabel("$z_{\\mathrm{inferred}}$")
-    plt.yticks([])
-
-    plt.show()
+    # plt.figure(figsize=(5, 5))
+    # plt.plot(lls)
+    # plt.show()
+    #
+    # plt.figure(figsize=(8, 8))
+    # _, state = rarhmm.viterbi(train_obs, train_act)
+    # _seq = npr.choice(len(train_obs))
+    #
+    # plt.subplot(211)
+    # plt.plot(train_obs[_seq])
+    # plt.xlim(0, len(train_obs[_seq]))
+    #
+    # plt.subplot(212)
+    # plt.imshow(state[_seq][None, :], aspect="auto", cmap=cmap, vmin=0, vmax=len(colors) - 1)
+    # plt.xlim(0, len(train_obs[_seq]))
+    # plt.ylabel("$z_{\\mathrm{inferred}}$")
+    # plt.yticks([])
+    #
+    # plt.show()
 
     # torch.save(rarhmm, open(rarhmm.trans_type + "_rarhmm_bouncing.pkl", "wb"))
 
-    hr = [20, 40, 60, 80, 100]
+    hr = [1, 20, 40, 60, 80, 100]
     for h in hr:
-        print(rarhmm.kstep_mse(obs[0:5], act[0:5], horizon=h, mix=False))
+        print("MSE: {0[0]}, EVAR:{0[1]}".format(rarhmm.kstep_mse(test_obs, test_act, horizon=h, mix=False)))
